02cdad431ac6b86729bb7e69416cdcc46626b995
omri wallach <omriwallach1992@gmail.com>
2019-03-15 20:07:30 +0100

    fetch data from server

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+interface IParsedResult {
+    payload: any;
+    results: any[];
+}
+
+interface IVendor {
+    displayName: string;
+    logo: {
+        "max-140x50": string;
+        original: string;
+    };
+    internalId: number;
+    imageSrc: string;
+    name: string;
+    price: number;
+    livingAreaTotal: number;
+}
+
+interface IAppProps {
+
+}
+
+interface IAppState {
+    vendors: IVendor[];
+}
+
+class App extends Component<IAppProps, IAppState> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            vendors:[]
+        }
+    }
+
+    componentDidMount() {
+        // fetch data
+        fetch("http://localhost:1337/houses")
+            .then(result => result.json())
+            .then((parsedResult: IParsedResult) => {
+                //fit vendor data
+                const vendors = parsedResult.results.map(vendor => this.getVendor(vendor));
+                //set state
+                this.setState({
+                    vendors
+                },
+                    () => console.log("state",this.state)
+                );
+            });
+    }
+
+    private getVendor(vendor: any): IVendor{
+        return {
+            displayName: vendor.vendor_verbose.display_name,
+            logo: vendor.vendor_verbose.logo,
+            internalId: vendor.internal_id,
+            name: vendor.name,
+            imageSrc: vendor.exterior_images[0]["fill-320x240"],
+            price: vendor.price,
+            livingAreaTotal: vendor.living_area_total
+        }
+    }
+        
+    
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
       </div>
     );
   }
